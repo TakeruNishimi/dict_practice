44516c3fbca96605bd0e61d1d58e42965c4ffc88
TakeruNishimi <takechu.nishimi@gmail.com>
2019-09-19 15:10:45 +0900

    ぶどう、みかん、りんごを表示

---
+++ fruits.py
@@ -0,0 +1,12 @@
+def main():
+    fruits_dict = {
+        'りんご': 'apple',
+        'ぶどう': 'Grape',
+        'みかん': 'Orange',
+    }
+    print(fruits_dict['りんご'])
+    print(fruits_dict['ぶどう'])
+    print(fruits_dict['みかん'])
+
+    if __name__ == '__main__':
+        main()
